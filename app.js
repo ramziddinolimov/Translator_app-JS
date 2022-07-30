870d0edd370397d0ed564563df0e21fd6f92b898
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-07-30 12:24:29 +0500

    countries country code

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const selectElement = document.querySelectorAll("select")
 
-selectElement.forEach(tag => {
+selectElement.forEach((tag, id) => {
     console.log(tag);
     for(const country_code in countries) {
+        let selected;
+        if(id == 0 && country_code == "en-GB") {
+            selected = "selected"
+        } else if (id == 1 && country_code == "hi-IN"){
+            selected = "selected"
+        }
         console.log(countries[country_code]);
-        let option = `<option value="${country_code}">${countries[country_code]}</option>`;
+        let option = `<option value="${country_code}"${selected}>${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option)
     }
 })

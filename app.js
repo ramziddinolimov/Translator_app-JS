7b4961ea1af76d4551bd996204f3668f673ba100
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-07-30 22:23:56 +0500

    icons upload

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const selectElement = document.querySelectorAll("select")
+const fromTextElement = document.querySelector(".from-text")
+const translateBtnElement = document.querySelector("button")
 
 selectElement.forEach((tag, id) => {
-    console.log(tag);
+    // console.log(tag);
     for(const country_code in countries) {
         let selected;
         if(id == 0 && country_code == "en-GB") {
@@ -9,8 +11,14 @@ selectElement.forEach((tag, id) => {
         } else if (id == 1 && country_code == "hi-IN"){
             selected = "selected"
         }
-        console.log(countries[country_code]);
+        // console.log(countries[country_code]);
         let option = `<option value="${country_code}"${selected}>${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option)
     }
+})
+
+
+translateBtnElement.addEventListener("click", () => {
+    let text = fromTextElement.value;
+    console.log(text);
 })
